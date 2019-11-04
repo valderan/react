771f137aee543dd1f880240d86adc54c03d8e2f5
git <v.kundryukov@gmail.com>
2019-11-04 23:39:13 +0300

    lesson 03 base

---
+++ src/components/post-list/post-list.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+import './post-list.css';
+import PostListItem from '../post-list-item';
+
+const PostList = () => {
+    return(
+        <ul className="app-list list-group">
+            <PostListItem/>
+            <PostListItem/>
+            <PostListItem/>
+        </ul>
+    )
+} 
+
+export default PostList;
