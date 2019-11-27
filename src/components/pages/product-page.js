d6b92041971d5484c91e96fd7b547e76441636ff
git <v.kundryukov@gmail.com>
2019-11-27 22:54:45 +0300

    lesson 16 base and hard: #1

--- src/components/pages/product-page.js
+++ src/components/pages/product-page.js
@@ -6,11 +6,9 @@ import MenuListItem from '../menu-list-item';
 class ProductPage extends React.Component {
     
     render() {
-       
         const { productId, menuItems } = this.props;
-    
         const menuItem = menuItems.filter( elem => elem.id === +productId);
-        console.log('menuItem: ', menuItem);
+        
         if (menuItem.length === 0) return <Error />
        
         return (    
